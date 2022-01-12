a6d3867ad880fbcf1099f283052a8799d8fe86ec
Kachros Dimitrios <77232998+kaxros95@users.noreply.github.com>
2022-01-12 15:35:28 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<head>
+
+// content before
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
+// or if you've downloaded the leafletjs files
+
+<link rel="stylesheet" href="<?= get_template_directory_uri() . '/path/to/leaflet.css' ?>" />
+<script src="<?= get_template_directory_uri() . '/path/to/leaflet.js' ?>"></script>
+
+// content after
+
+</head>
